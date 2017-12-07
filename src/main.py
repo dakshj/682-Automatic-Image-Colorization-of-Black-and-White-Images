7f9d24f3afc28c0fcd108b0dce34c9ed3982512e
Daksh Jotwani <dakshjay@gmail.com>
2017-12-07 15:09:30 -0500

    Use PIL.Image to load images

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 import os
 
 import numpy as np
+from PIL import Image
 from keras import Model
 from keras.engine.topology import Input
-from keras.preprocessing.image import img_to_array, load_img
 
 from decoder import init_decoder
 from encoder import init_encoder
@@ -17,16 +17,13 @@ def train():
 def load_image_data(folder_path, normalize=True):
     array = np.array(  # Array of all images
         [np.array(
-
-            # Use keras to load image as an array
-            img_to_array(load_img(
-
+            Image.open(
                 # Get full path of image file
-                os.path.join(folder_path, file_name)
-            )))
+                os.path.join(folder_path, file_name)))
 
             # Repeat this for all files in the folder
-            for file_name in os.listdir(folder_path)])
+            for file_name in os.listdir(folder_path)]
+    )
 
     if normalize:
         # Normalize all pixel values from 0-255 to 0-1.0
