e03369de7fdc70c131cdf5aa177d2bd4c50501e4
Daksh Jotwani <dakshjay@gmail.com>
2017-12-06 21:23:11 -0500

    Add normalization to training data

--- src/main.py
+++ src/main.py
@@ -4,14 +4,29 @@ import numpy as np
 from keras.preprocessing.image import img_to_array, load_img
 
 
-def get_normalized_training_data(folder_path):
-    return np.array([np.array(img_to_array(load_img(os.path.join(folder_path, file_name))))
-                     for file_name in os.listdir(folder_path)])
-
-
 def train():
-    X_train = get_normalized_training_data('../dataset/train')
-    print(X_train)
+    X_train = load_image_data('../dataset/train', normalize=True)
+
+
+def load_image_data(folder_path, normalize=True):
+    array = np.array(  # Array of all images
+        [np.array(
+
+            # Use keras to load image as an array
+            img_to_array(load_img(
+
+                # Get full path of image file
+                os.path.join(folder_path, file_name)
+            )))
+
+            # Repeat this for all files in the folder
+            for file_name in os.listdir(folder_path)])
+
+    if normalize:
+        # Normalize all pixel values from 0-255 to 0-1.0
+        return 1.0 / 255 * array
+
+    return array
 
 
 if __name__ == '__main__':
